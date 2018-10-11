4f80e75a97c363e512a38da506d0284bec38b31e
MicahWeiss <micahjweiss@gmail.com>
2018-10-10 19:39:47 -0500

    stuck on broken promises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if(err){
+      callback(err, data);
+    } else {
+      let firstLine = data.split('\n')[0];
+      callback(err, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  request.get(url, (err, data) => {
+    if (err) {
+      callback(err, data);
+    } else {
+      callback(err, data.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -3,20 +3,16 @@
  * Any successful value should be made available in the next `then` block chained
  * to the function invocation, while errors should be available in the `catch` block
  */
-
+var nodeStyle = require('./callbackReview.js');
 var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
-};
+var pluckFirstLineFromFileAsync = Promise.promisify(nodeStyle.pluckFirstLineFromFile);
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCodeAsync = function(url) {
-  // TODO
-};
+var getStatusCodeAsync = Promise.promisify(nodeStyle.getStatusCode)
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
